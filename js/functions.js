c1a48ea7e40d1c9d4436a7ed4eb34834c64c0e5b
julsco <juliancscotti@gmail.com>
2022-08-30 20:28:57 +0100

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+function formSubmit(){
+    alert("Your message has been sent, we will get back to you as soon as possible!");
+}
+
+function validateName(inputtxt)
+      { 
+      var letters = /^[A-Za-z]+$/;
+      if(inputtxt.value.match(letters))
+      {
+      alert('Your name have accepted : you can try another');
+      return true;
+      }
+      else
+      {
+      alert('Please input alphabet characters only');
+      return false;
+      }
+      }
